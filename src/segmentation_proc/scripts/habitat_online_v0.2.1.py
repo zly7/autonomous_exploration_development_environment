cb07ba38b10d417d195bd6fbf14a715bd2fb6f44
Ji Zhang <zhangji@cmu.edu>
2022-11-23 19:57:25 -0500

    update python scripts

--- src/segmentation_proc/scripts/habitat_online_v0.2.1.py
+++ src/segmentation_proc/scripts/habitat_online_v0.2.1.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import rclpy
+from rclpy.node import Node
 import sys
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Image as ROS_Image
@@ -114,11 +115,60 @@ class ABTestGroup(Enum):
     CONTROL = 1
     TEST = 2
 
-class DemoRunner:
+class DemoRunner(Node):
     def __init__(self, sim_settings, simulator_demo_type):
+        super().__init__('habitat_online')
+
         if simulator_demo_type == DemoRunnerType.EXAMPLE:
             self.set_sim_settings(sim_settings)
+
+        start_state = self.init_common()
+
         self._demo_type = simulator_demo_type
+        self.time = 0;
+        self.time_stamp = self.get_clock().now().to_msg()   
+        self.camera_roll = 0
+        self.camera_pitch = 0
+        self.camera_yaw = 0
+        self.camera_x = 0
+        self.camera_y = 0
+        self.camera_z = 0.5
+
+        self.create_subscription(Odometry, '/state_estimation', self.state_estimation_callback,2)
+
+        if self._sim_settings["color_sensor"]:
+            self.color_image_pub = self.create_publisher(ROS_Image,"/habitat_camera/color/image", 2)
+            self.color_image = ROS_Image()
+            self.color_image.header.frame_id = "habitat_camera"
+            self.color_image.height = settings["height"]
+            self.color_image.width  = settings["width"]
+            self.color_image.encoding = "rgb8"
+            self.color_image.step = 3 * self.color_image.width
+            self.color_image.is_bigendian = False
+
+        if self._sim_settings["depth_sensor"]:
+            self.depth_image_pub = self.create_publisher(ROS_Image,"/habitat_camera/depth/image", 2)
+            self.depth_image = ROS_Image()
+            self.depth_image.header.frame_id = "habitat_camera"
+            self.depth_image.height = settings["height"]
+            self.depth_image.width  = settings["width"]
+            self.depth_image.encoding = "mono8"
+            self.depth_image.step = self.color_image.width
+            self.depth_image.is_bigendian = False
+
+        if self._sim_settings["semantic_sensor"]:
+            self.semantic_image_pub = self.create_publisher(ROS_Image,"/habitat_camera/semantic/image", 2)
+            self.semantic_image = ROS_Image()
+            self.semantic_image.header.frame_id = "habitat_camera"
+            self.semantic_image.height = settings["height"]
+            self.semantic_image.width  = settings["width"]
+            self.semantic_image.encoding = "rgb8"
+            self.semantic_image.step = 3 * self.color_image.width
+            self.semantic_image.is_bigendian = False
+
+        rate = default_sim_settings["frame_rate"]
+        timer_period = 1 / rate 
+        self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def set_sim_settings(self, sim_settings):
         self._sim_settings = sim_settings.copy()
@@ -165,87 +215,50 @@ class DemoRunner:
         self.camera_y = msg.pose.pose.position.y
         self.camera_z = msg.pose.pose.position.z
 
-    def listener(self):
-        start_state = self.init_common()
+    def timer_callback(self):
+        roll = -self.camera_roll
+        pitch = self.camera_pitch
+        yaw = 1.5708 - self.camera_yaw
 
-        rclpy.init(args=sys.argv)
-        node = rclpy.create_node('habitat_online')
-        node.create_subscription(Odometry, '/state_estimation', self.state_estimation_callback,2)
-        self.time = 0;
-        self.time_stamp = node.get_clock().now().to_msg()   
-        self.camera_roll = 0
-        self.camera_pitch = 0
-        self.camera_yaw = 0
-        self.camera_x = 0
-        self.camera_y = 0
-        self.camera_z = 0.5
+        qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+        qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+        qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+        qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+
+        position = np.array([self.camera_x, self.camera_y, self.camera_z])
+        position[1], position[2] = position[2], -position[1]
+        
+        agent_state = self._sim.get_agent(0).get_state()
+        for sensor in agent_state.sensor_states:
+            agent_state.sensor_states[sensor].position = position + np.array([0, default_sim_settings["camera_offset_z"], 0])
+            agent_state.sensor_states[sensor].rotation = quat_from_coeffs(np.array([-qy, -qz, qx, qw]))
+
+        self._sim.get_agent(0).set_state(agent_state, infer_sensor_states = False)
+        observations = self._sim.step("move_forward")
 
         if self._sim_settings["color_sensor"]:
-            self.color_image_pub = node.create_publisher(ROS_Image,"/habitat_camera/color/image", 2)
-            self.color_image = ROS_Image()
-            self.color_image.header.frame_id = "habitat_camera"
-            self.color_image.height = settings["height"]
-            self.color_image.width  = settings["width"]
-            self.color_image.encoding = "rgb8"
-            self.color_image.step = 3 * self.color_image.width
-            self.color_image.is_bigendian = False
-
+            self.publish_color_observation(observations)
         if self._sim_settings["depth_sensor"]:
-            self.depth_image_pub = node.create_publisher(ROS_Image,"/habitat_camera/depth/image", 2)
-            self.depth_image = ROS_Image()
-            self.depth_image.header.frame_id = "habitat_camera"
-            self.depth_image.height = settings["height"]
-            self.depth_image.width  = settings["width"]
-            self.depth_image.encoding = "mono8"
-            self.depth_image.step = self.color_image.width
-            self.depth_image.is_bigendian = False
-
+            self.publish_depth_observation(observations)
         if self._sim_settings["semantic_sensor"]:
-            self.semantic_image_pub = node.create_publisher(ROS_Image,"/habitat_camera/semantic/image", 2)
-            self.semantic_image = ROS_Image()
-            self.semantic_image.header.frame_id = "habitat_camera"
-            self.semantic_image.height = settings["height"]
-            self.semantic_image.width  = settings["width"]
-            self.semantic_image.encoding = "rgb8"
-            self.semantic_image.step = 3 * self.color_image.width
-            self.semantic_image.is_bigendian = False
+            self.publish_semantic_observation(observations)
 
-        r = node.create_rate(default_sim_settings["frame_rate"])
-        while rclpy.ok():
-            rclpy.spin_once(node)
-            roll = -self.camera_roll
-            pitch = self.camera_pitch
-            yaw = 1.5708 - self.camera_yaw
+        state = self._sim.last_state()
 
-            qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
-            qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
-            qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
-            qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+def main(args=None):
+    rclpy.init(args=args)
 
-            position = np.array([self.camera_x, self.camera_y, self.camera_z])
-            position[1], position[2] = position[2], -position[1]
-            
-            agent_state = self._sim.get_agent(0).get_state()
-            for sensor in agent_state.sensor_states:
-                agent_state.sensor_states[sensor].position = position + np.array([0, default_sim_settings["camera_offset_z"], 0])
-                agent_state.sensor_states[sensor].rotation = quat_from_coeffs(np.array([-qy, -qz, qx, qw]))
+    demo_runner = DemoRunner(settings, DemoRunnerType.EXAMPLE)
 
-            self._sim.get_agent(0).set_state(agent_state, infer_sensor_states = False)
-            observations = self._sim.step("move_forward")
+    rclpy.spin(demo_runner)
 
-            if self._sim_settings["color_sensor"]:
-                self.publish_color_observation(observations)
-            if self._sim_settings["depth_sensor"]:
-                self.publish_depth_observation(observations)
-            if self._sim_settings["semantic_sensor"]:
-                self.publish_semantic_observation(observations)
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    demo_runner.destroy_node()
+    rclpy.shutdown()
+    demo_runner._sim.close()
+    del demo_runner._sim
 
-            state = self._sim.last_state()
-
-        node.destroy_node()
-        rclpy.shutdown()
-        self._sim.close()
-        del self._sim
-
-demo_runner = DemoRunner(settings, DemoRunnerType.EXAMPLE)
-demo_runner.listener()
+if __name__ == '__main__':
+    main()
